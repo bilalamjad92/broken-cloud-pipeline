f49b9762d40740f9aa420bdf1af13a978d3fc283
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-26 18:38:18 +0000

    Update provider.tf

--- broken-cloud-pipeline/terraform/provider.tf
+++ broken-cloud-pipeline/terraform/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Compatible with modern practices
+      version = "~> 5.0" 
     }
   }
 }
 
 provider "aws" {
-  region = "eu-central-1" # Frankfurt region per your requirement
+  region = "eu-central-1" 
   default_tags {
     tags = var.tags
   }
