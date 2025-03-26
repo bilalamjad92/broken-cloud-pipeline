cd05bc3fc8692bbe2d1d2cf6097b1201cf25f1b2
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-26 18:43:18 +0000

    Update main.tf

--- broken-cloud-pipeline/terraform/modules/monitoring/main.tf
+++ broken-cloud-pipeline/terraform/modules/monitoring/main.tf
@@ -2,13 +2,11 @@ resource "aws_sns_topic" "this" {
   name = "${var.name}-notifications"
   tags = { Component = "SNS" }
 }
-
 resource "aws_sns_topic_subscription" "email" {
   topic_arn = aws_sns_topic.this.arn
   protocol  = "email"
   endpoint  = var.email
 }
-
 resource "aws_cloudwatch_metric_alarm" "cost_alarm" {
   alarm_name          = "${var.name}-cost-alarm"
   metric_name         = "EstimatedCharges"
@@ -21,7 +19,6 @@ resource "aws_cloudwatch_metric_alarm" "cost_alarm" {
   alarm_actions       = [aws_sns_topic.this.arn]
   tags                = { Purpose = "Cost Monitoring" }
 }
-
 resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
   alarm_name          = "${var.name}-alb-5xx-alarm"
   metric_name         = "HTTPCode_ELB_5XX_Count"
@@ -36,4 +33,28 @@ resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
     LoadBalancer = var.alb_arn_suffix
   }
   tags = { Purpose = "ALB Health" }
+}
+resource "aws_cloudwatch_metric_alarm" "app_health" {
+  alarm_name          = "${var.name}-app-health"
+  metric_name         = "HealthCheckStatus"
+  namespace           = "AWS/Route53"
+  statistic           = "Average"
+  period              = 60
+  evaluation_periods  = 2
+  threshold           = 1
+  comparison_operator = "LessThanThreshold"
+  alarm_actions       = [aws_sns_topic.this.arn]
+  dimensions = { HealthCheckId = var.app_health_check }
+}
+resource "aws_cloudwatch_metric_alarm" "jenkins_health" {
+  alarm_name          = "${var.name}-jenkins-health"
+  metric_name         = "HealthCheckStatus"
+  namespace           = "AWS/Route53"
+  statistic           = "Average"
+  period              = 60
+  evaluation_periods  = 2
+  threshold           = 1
+  comparison_operator = "LessThanThreshold"
+  alarm_actions       = [aws_sns_topic.this.arn]
+  dimensions = { HealthCheckId = var.jenkins_health_check }
 }
