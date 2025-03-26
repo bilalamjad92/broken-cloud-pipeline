73c416758de83aba7d497aa5017c8a6d46f60c23
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-26 18:39:46 +0000

    Update variables.tf

--- broken-cloud-pipeline/terraform/modules/alb/variables.tf
+++ broken-cloud-pipeline/terraform/modules/alb/variables.tf
@@ -27,12 +27,3 @@ variable "log_bucket_policy_id" {
   description = "ID of the S3 bucket policy"
   type        = string
 }
-# variable "name" { type = string }
-# variable "vpc_id" { type = string }
-# variable "public_subnets" { type = list(string) }
-# variable "allowed_cidr" { type = list(string) }
-# variable "tags" { type = object({}) }
-# variable "log_bucket_id" {
-#   description = "ID of the S3 bucket for ALB access logs"
-#   type        = string
-# }
