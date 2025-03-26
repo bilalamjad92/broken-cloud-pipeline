d5785da25d806f7b30db744e0446a13673e6753e
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-26 18:44:45 +0000

    Update variables.tf

--- broken-cloud-pipeline/terraform/modules/monitoring/variables.tf
+++ broken-cloud-pipeline/terraform/modules/monitoring/variables.tf
@@ -2,13 +2,13 @@ variable "name" {
   type        = string
   description = "Name prefix for monitoring resources"
 }
-
 variable "email" {
   type        = string
   description = "Email address for SNS notifications"
 }
-
 variable "alb_arn_suffix" {
   type        = string
   description = "ARN suffix of the ALB for monitoring"
 }
+variable "app_health_check" { type = string }
+variable "jenkins_health_check" { type = string }
