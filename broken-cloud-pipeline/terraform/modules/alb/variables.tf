3f5eceeba872ffe3a2ab95de8fddf023e3279e9d
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-27 04:11:54 +0000

    Update variables.tf

--- broken-cloud-pipeline/terraform/modules/alb/variables.tf
+++ broken-cloud-pipeline/terraform/modules/alb/variables.tf
@@ -27,3 +27,7 @@ variable "log_bucket_policy_id" {
   description = "ID of the S3 bucket policy"
   type        = string
 }
+variable "certificate_arn" {
+  default     = "arn:aws:acm:eu-central-1:216989105561:certificate/15556a87-717c-44e7-8d68-aaf146333c17"
+  description = "ARN of the ACM certificate"
+}
