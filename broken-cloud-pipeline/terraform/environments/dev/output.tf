58a434ce53471d6388098bf457c59ca894b028a9
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-27 03:42:31 +0000

    Update output.tf

--- broken-cloud-pipeline/terraform/environments/dev/output.tf
+++ broken-cloud-pipeline/terraform/environments/dev/output.tf
@@ -5,3 +5,12 @@ output "app_alb_dns" {
 output "jenkins_alb_dns" {
   value = module.jenkins_alb.alb_dns_name
 }
+
+output "target_group_arn" {
+  value = module.app_alb.target_group_arn
+}
+
+output "alb_security_group_id" {
+  value = module.app_alb.alb_security_group_id
+}
+
