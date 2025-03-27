947da93a95097992f87022f6e8505c7750cd1cee
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-27 04:16:30 +0000

    Update main.tf

--- broken-cloud-pipeline/terraform/modules/vpc/main.tf
+++ broken-cloud-pipeline/terraform/modules/vpc/main.tf
@@ -1,17 +1,51 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
-
   name = "${var.name}-vpc"
   cidr = var.cidr
-
   azs             = ["eu-central-1a", "eu-central-1b"]
-  private_subnets = [cidrsubnet(var.cidr, 8, 1), cidrsubnet(var.cidr, 8, 2)] # e.g., 10.40.1.0/24, 10.40.2.0/24
-  public_subnets  = [cidrsubnet(var.cidr, 8, 3), cidrsubnet(var.cidr, 8, 4)] # e.g., 10.40.3.0/24, 10.40.4.0/24
-
-  enable_nat_gateway = true
-  single_nat_gateway = true # Cost minimization
+  private_subnets = [cidrsubnet(var.cidr, 8, 1), cidrsubnet(var.cidr, 8, 2)] 
+  public_subnets  = [cidrsubnet(var.cidr, 8, 3), cidrsubnet(var.cidr, 8, 4)] 
+  enable_nat_gateway   = true
+  single_nat_gateway   = true # Cost minimization
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags                 = var.tags
+}
+resource "aws_network_acl" "public" {
+  vpc_id     = module.vpc.vpc_id     
+  subnet_ids = module.vpc.public_subnets
+  ingress {
+    rule_no    = 100
+    action     = "allow"
+    from_port  = 443
+    to_port    = 443
+    protocol   = "tcp"
+    cidr_block = "0.0.0.0/0"
+  }
+  ingress {
+    rule_no    = 200
+    action     = "allow"
+    from_port  = 1024
+    to_port    = 65535
+    protocol   = "tcp"
+    cidr_block = "0.0.0.0/0"
+  }
+  ingress {
+    rule_no    = 300
+    action     = "deny"
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_block = "0.0.0.0/0"
+  }
+  egress {
+    rule_no    = 100
+    action     = "allow"
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_block = "0.0.0.0/0"
+  }
+  tags = merge(var.tags, { Name = "${var.name}-public-nacl" })
 }
