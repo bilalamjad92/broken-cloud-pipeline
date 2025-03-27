5596eb31de322acb4ce33ae0eae6c080828ca1eb
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-27 12:07:41 +0000

    Update main.tf

--- broken-cloud-pipeline/terraform/modules/ecs_cluster/main.tf
+++ broken-cloud-pipeline/terraform/modules/ecs_cluster/main.tf
@@ -2,13 +2,12 @@ resource "aws_ecs_cluster" "this" {
   name = "${var.name}-cluster"
   tags = var.tags
 }
-
 resource "aws_security_group" "ecs_instance_sg" {
   name_prefix = "${var.name}-ecs-sg-"
   vpc_id      = var.vpc_id
   ingress {
-    from_port       = 8080
-    to_port         = 8080
+    from_port       = 32768
+    to_port         = 65535
     protocol        = "tcp"
     security_groups = [var.alb_security_group_id]
   }
@@ -20,7 +19,6 @@ resource "aws_security_group" "ecs_instance_sg" {
   }
   tags = var.tags
 }
-
 resource "aws_iam_role" "ecs_instance_role" {
   name = "${var.name}-ecs-instance-role"
   assume_role_policy = jsonencode({
@@ -33,21 +31,17 @@ resource "aws_iam_role" "ecs_instance_role" {
   })
   tags = var.tags
 }
-
 resource "aws_iam_role_policy_attachment" "ecs_instance_policy" {
   role       = aws_iam_role.ecs_instance_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
-
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
   name = "${var.name}-ecs-instance-profile"
   role = aws_iam_role.ecs_instance_role.name
 }
-
 data "aws_ssm_parameter" "ecs_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
-
 resource "aws_launch_template" "ecs" {
   name_prefix            = "${var.name}-ecs-"
   image_id               = data.aws_ssm_parameter.ecs_ami.value
@@ -61,7 +55,6 @@ resource "aws_launch_template" "ecs" {
   )
   tags = var.tags
 }
-
 resource "aws_autoscaling_group" "ecs" {
   name_prefix         = "${var.name}-ecs-asg-"
   vpc_zone_identifier = var.private_subnets # Place in private subnets
@@ -83,8 +76,6 @@ resource "aws_autoscaling_group" "ecs" {
     propagate_at_launch = true
   }
 }
-
-
 resource "aws_ecs_capacity_provider" "this" {
   name = "${var.name}-ecs-cp"
   auto_scaling_group_provider {
@@ -97,7 +88,6 @@ resource "aws_ecs_capacity_provider" "this" {
     }
   }
 }
-
 resource "aws_ecs_cluster_capacity_providers" "this" {
   cluster_name       = aws_ecs_cluster.this.name
   capacity_providers = [aws_ecs_capacity_provider.this.name]
@@ -107,8 +97,6 @@ resource "aws_ecs_cluster_capacity_providers" "this" {
     weight            = 100
   }
 }
-
-
 resource "aws_ecs_task_definition" "this" {
   family       = "${var.name}-task"
   network_mode = "bridge"
@@ -118,7 +106,7 @@ resource "aws_ecs_task_definition" "this" {
     cpu    = var.cpu
     memory = 512
     portMappings = [{
-      containerPort = 8080 # Changed to 8080 for both jenkins and hello-world
+      containerPort = var.name == "jenkins" ? 8080 : 80 # Changed to 8080 for both jenkins and hello-world
       hostPort      = 0
       protocol      = "tcp"
     }]
@@ -138,17 +126,16 @@ resource "aws_ecs_service" "this" {
   cluster                           = aws_ecs_cluster.this.id
   task_definition                   = aws_ecs_task_definition.this.arn
   desired_count                     = var.container_count
-  health_check_grace_period_seconds = 30 # Give 30s for container to start
+  health_check_grace_period_seconds = 300 # Give 300s for container to start
   load_balancer {
     target_group_arn = var.target_group_arn # Passed from ALB module
     container_name   = var.container_name
-    container_port = 8080
+    container_port = var.name == "jenkins" ? 8080 : 80
   }
   capacity_provider_strategy {
     capacity_provider = aws_ecs_capacity_provider.this.name
     weight            = 1
   }
-
   depends_on = [var.target_group_arn, aws_ecs_cluster_capacity_providers.this] # Ensure ALB is ready
 }
 resource "aws_cloudwatch_log_group" "ecs" {
