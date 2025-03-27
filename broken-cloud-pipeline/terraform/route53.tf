91463f4801d6cafedbb4ef46d90f9ebec6f6123e
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-27 03:49:29 +0000

    Update route53.tf

--- broken-cloud-pipeline/terraform/route53.tf
+++ broken-cloud-pipeline/terraform/route53.tf
@@ -1,6 +1,7 @@
 resource "aws_route53_health_check" "app" {
-  port              = 80
-  type              = "HTTP"
+  # fqdn              = module.app_alb.alb_dns_name
+  port              = 443
+  type              = "HTTPS"
   resource_path     = "/"
   request_interval  = 30
   failure_threshold = 3
@@ -8,11 +9,41 @@ resource "aws_route53_health_check" "app" {
 }
 
 resource "aws_route53_health_check" "jenkins" {
-  port              = 80
-  type              = "HTTP"
+  #  fqdn              = module.jenkins_alb.alb_dns_name
+  port              = 443
+  type              = "HTTPS"
   resource_path     = "/login"
   request_interval  = 30
   failure_threshold = 3
   tags              = { Component = "Route53" }
+}
+# Route 53 Record for app
+resource "aws_route53_record" "app" {
+  zone_id = aws_route53_zone.mptchallenge.zone_id
+  name    = "app.mptchallenge.local"
+  type    = "A"
+
+  alias {
+    name                   = module.app_alb.alb_dns_name
+    zone_id                = module.app_alb.alb_zone_id
+    evaluate_target_health = true
+  }
+
+  health_check_id = aws_route53_health_check.app.id
+}
+
+# Route 53 Record for jenkins
+resource "aws_route53_record" "jenkins" {
+  zone_id = aws_route53_zone.mptchallenge.zone_id
+  name    = "jenkins.mptchallenge.local"
+  type    = "A"
+
+  alias {
+    name                   = module.jenkins_alb.alb_dns_name
+    zone_id                = module.jenkins_alb.alb_zone_id
+    evaluate_target_health = true
+  }
+
+  health_check_id = aws_route53_health_check.jenkins.id
 }
 #commented
