4e95966c4ff0174fa6c77592448f2ab092a73948
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-27 15:49:16 +0000

    Update route53.tf: app /health check

--- broken-cloud-pipeline/terraform/route53.tf
+++ broken-cloud-pipeline/terraform/route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_health_check" "app" {
   port              = 443
   type              = "HTTPS"
-  resource_path     = "/"
+  resource_path     = "/health"
   request_interval  = 30
   failure_threshold = 3
   tags              = { Component = "Route53" }
@@ -15,11 +15,12 @@ resource "aws_route53_health_check" "jenkins" {
   failure_threshold = 3
   tags              = { Component = "Route53" }
 }
-
+# Route 53 Record for app
 resource "aws_route53_record" "app" {
   zone_id = aws_route53_zone.mptchallenge.zone_id
   name    = "app.mptchallenge.local"
   type    = "A"
+
   alias {
     name                   = module.app_alb.alb_dns_name
     zone_id                = module.app_alb.alb_zone_id
@@ -28,14 +29,19 @@ resource "aws_route53_record" "app" {
 
   health_check_id = aws_route53_health_check.app.id
 }
+
+# Route 53 Record for jenkins
 resource "aws_route53_record" "jenkins" {
   zone_id = aws_route53_zone.mptchallenge.zone_id
   name    = "jenkins.mptchallenge.local"
   type    = "A"
+
   alias {
     name                   = module.jenkins_alb.alb_dns_name
     zone_id                = module.jenkins_alb.alb_zone_id
     evaluate_target_health = true
   }
+
   health_check_id = aws_route53_health_check.jenkins.id
 }
+
