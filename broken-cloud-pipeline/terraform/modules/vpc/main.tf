98d59026043d5c630ea5933a915a41cf7c159ba5
bilalamjad92 <118302569+bilalamjad92@users.noreply.github.com>
2025-03-27 15:52:18 +0000

    Update main.tf

--- broken-cloud-pipeline/terraform/modules/vpc/main.tf
+++ broken-cloud-pipeline/terraform/modules/vpc/main.tf
@@ -1,20 +1,26 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
+
   name = "${var.name}-vpc"
   cidr = var.cidr
+
   azs             = ["eu-central-1a", "eu-central-1b"]
   private_subnets = [cidrsubnet(var.cidr, 8, 1), cidrsubnet(var.cidr, 8, 2)] 
   public_subnets  = [cidrsubnet(var.cidr, 8, 3), cidrsubnet(var.cidr, 8, 4)] 
+
   enable_nat_gateway   = true
   single_nat_gateway   = true # Cost minimization
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags                 = var.tags
 }
+
 resource "aws_network_acl" "public" {
   vpc_id     = module.vpc.vpc_id     
   subnet_ids = module.vpc.public_subnets
+
+  # Allow HTTPS inbound
   ingress {
     rule_no    = 100
     action     = "allow"
@@ -23,6 +29,8 @@ resource "aws_network_acl" "public" {
     protocol   = "tcp"
     cidr_block = "0.0.0.0/0"
   }
+
+  # Allow HTTP inbound
   ingress {
     rule_no    = 150
     action     = "allow"
@@ -31,6 +39,8 @@ resource "aws_network_acl" "public" {
     protocol   = "tcp"
     cidr_block = "0.0.0.0/0"
   }
+
+  # Allow ephemeral ports for return traffic
   ingress {
     rule_no    = 200
     action     = "allow"
@@ -39,6 +49,8 @@ resource "aws_network_acl" "public" {
     protocol   = "tcp"
     cidr_block = "0.0.0.0/0"
   }
+
+  # Deny all other inbound
   ingress {
     rule_no    = 300
     action     = "deny"
@@ -47,6 +59,8 @@ resource "aws_network_acl" "public" {
     protocol   = "-1"
     cidr_block = "0.0.0.0/0"
   }
+
+  # Allow all outbound
   egress {
     rule_no    = 100
     action     = "allow"
@@ -55,5 +69,6 @@ resource "aws_network_acl" "public" {
     protocol   = "-1"
     cidr_block = "0.0.0.0/0"
   }
+
   tags = merge(var.tags, { Name = "${var.name}-public-nacl" })
 }
